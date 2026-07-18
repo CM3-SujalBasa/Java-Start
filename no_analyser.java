ebf3919c42a15649659c8ea9ad6d162b68a95d16
CM3-SujalBasa <sujalbasa2005@gmail.com>
2026-07-18 20:00:34 +0530

    this is the no analyser code

--- no_analyser.java
+++ no_analyser.java
@@ -15,24 +15,29 @@ class no_analyser{
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the no: ");
         int no= sc.nextInt();
-        
+        String s="";
         if (no==0) {
             System.out.print(no+" is equal to zero");
         }else{
-            String s = (no>0)?  ((no%2==0)? no+" +ve Even" : no+" +ve Odd") : ((no%2==0)? no+" -ve Even" : no+" -ve Odd"));
+            s = (no>0)?  ((no%2==0)? no+" +ve Even" : no+" +ve Odd") : ((no%2==0)? no+" -ve Even" : no+" -ve Odd");
         }
+        System.out.println(s);
 
-        System.out.print(((no%5==0)&&(no%11==0))? " is divisible by 5 and 11": "is not divisible by 5 and 11");
+        System.out.println(((no%5==0)&&(no%11==0))? no+" is divisible by 5 and 11": no+"is not divisible by 5 and 11");
 
         f1(no); //to find the no of digits
     }
     public static void f1(int n) {
-        int cc=0;
-        while (n>0) {
-            n=n%10;
-            cc+=1;
-            n/=10;
+        int r=0,cc=0,n1=Math.abs(n);
+        if (n1==0) {
+            System.out.println(n1+" is a 1 digit no");
+        }else{
+            while (n1>0) {
+                // r=n1%10; // here this step can be not used bcz here we dont want the remainder means the last digit
+                cc+=1;
+                n1/=10;
+            }
         }
-        System.out.print(n+" is a"+cc+" digit no");
+        System.out.print(n+" is a "+cc+" digit no");
     }
 }
