c9c6aec6332c5ceeb0beb35b2f02c65e53a60be6
CM3-SujalBasa <sujalbasa2005@gmail.com>
2026-07-18 22:45:35 +0530

    this was quite tough

--- char_analyser.java
+++ char_analyser.java
@@ -19,15 +19,25 @@ class char_analyser{
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the charater(only one charater): ");
         int no= sc.next().charAt(0);
-        
-        System.out.print((no>=65 && no<=90)? Integer.toString(no)+" Uppercase character":"");
-        System.out.print((no>=97 && no<=122)? Integer.toString(no)+" Lowercase character":"");
-        System.out.print((no>=32 && no<=47) || (no>=58 && no<=64) || (no>=91 && no<=96) || (no>=123 && no<=127 || no==0 || no==9 || no==10 || no==13 || no==27)? "\n"+Integer.toString(no)+" Special character (Symbols)":"");
-        System.out.print((no>=48 && no<=57)? Integer.toString(no)+" Digits":"");
+        boolean f=false;
+        System.out.print((no>=65 && no<=90)? (char)(no)+" Uppercase character":"");
+        System.out.print((no>=97 && no<=122)? (char)(no)+" Lowercase character":"");
+        System.out.print((no>=32 && no<=47) || (no>=58 && no<=64) || (no>=91 && no<=96) || (no>=123 && no<=127 || no==0 || no==9 || no==10 || no==13 || no==27)? "\n"+(char)(no)+" Special character (Symbols)":"");
+        System.out.print((no>=48 && no<=57)? (char)(no)+" Digits":"");
         int arr[]={65,69,73,79,85,97,101,105,111,117};
-        for (int i : arr) {
-            if (i==no) {
-                System.out.print("\n"+no+" is a vowel");
+
+        for (int j : arr) {
+            if (j==no) {
+                f=true;
+                break;
+            }
+        }
+        if ((no>=65 && no<=90) || (no>=97 && no<=122)){
+            if (f==true) {
+                System.out.print("\n"+(char)no+" is a vowel");
+            }
+            else{
+                System.out.print("\n"+(char)no+" is a Consonant");
             }
         }
     }
